c242a34c7c8531978c15ec3ba0ce94f5b8426301
SakshamL <sakshamluthra2016@gmail.com>
2024-01-11 02:52:14 +0530

    UI Updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const MovieCallAPI = async () => {
     const APIURL =
-      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=10";
 
     const response = await fetch(APIURL);
     const responseJSON = await response.json();
