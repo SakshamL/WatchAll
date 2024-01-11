a01207a82588570acc65d613043285df693b420f
SakshamL <sakshamluthra2016@gmail.com>
2024-01-11 20:56:08 +0530

    null posters update

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,12 +4,18 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const MovieList = (props) => {
   return (
     <>
-      {props.movies.map((movie, index) => (
-        <div key={index} className="movie-block">
-          <img src={IMGPATH + movie.poster_path} alt="movie"></img>
-          <p>{movie.title}</p>
-        </div>
-      ))}
+      {props.movies.map((movie, index) => {
+        if (movie.poster_path !== null) {
+          return (
+            <div key={index} className="movie-block">
+              <img src={IMGPATH + movie.poster_path} alt="movie"></img>
+              <p>{movie.title}</p>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
     </>
   );
 };
